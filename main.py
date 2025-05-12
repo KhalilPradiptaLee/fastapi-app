6df3a982dd686c16c3b314c9bcd15851d8c0081e
KhalilPradiptaLee <144757000+KhalilPradiptaLee@users.noreply.github.com>
2025-05-12 16:50:47 +0800

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from pydantic import BaseModel
 import pickle
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
 # Load model
 with open("expsmooth_model.pkl", "rb") as f:
