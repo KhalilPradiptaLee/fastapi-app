f532ade5263a724c68c4bfead15cc306c5aa7176
KhalilPradiptaLee <144757000+KhalilPradiptaLee@users.noreply.github.com>
2025-05-12 15:46:52 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
+# Load model
+with open("expsmooth_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = FastAPI(
+    title="API Prediksi Pemakaian Solar",
+    description="Prediksi jumlah pemakaian solar menggunakan model Exponential Smoothing berdasarkan input jumlah hari ke depan.",
+    version="1.0.0"
+)
+
+class InputData(BaseModel):
+    steps: int  # jumlah hari ke depan
+
+@app.post("/predict")
+def predict(data: InputData):
+    forecast = model.forecast(data.steps)
+    
+    # Ambil tanggal sekarang dengan zona waktu Samarinda (WITA / Asia/Makassar)
+    current_date = datetime.now(ZoneInfo("Asia/Makassar"))
+    
+    prediction_list = []
+    for i, value in enumerate(forecast):
+        forecast_date = current_date + timedelta(days=i)
+        formatted_date = forecast_date.strftime("%Y-%m-%d")
+        prediction_list.append(f"Prediksi pada {formatted_date} : {round(value, 2)} KL")
+    
+    total = round(sum(forecast), 2)
+    total_text = f"Total prediksi selama {data.steps} hari adalah: {total} KL"
+    
+    prediction_list.append(total_text)
+
+    return {
+        "prediction": prediction_list
+    }
